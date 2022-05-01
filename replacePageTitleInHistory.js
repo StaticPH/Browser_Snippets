d43553292aa47a6de6ab7a062f0dea59d1837de7
StaticPH <7786502+StaticPH@users.noreply.github.com>
2022-05-01 14:57:26 -0400

    replacePageTitleInHistory.js: Do nothing if new title is unspecified/a zero-length string

--- replacePageTitleInHistory.js
+++ replacePageTitleInHistory.js
@@ -1,5 +1,10 @@
 //Replace the title of the current page in your browser history
 (function (newTitle){
-	document.head.querySelector('title').text = newTitle;
-	history.replaceState(history.state, newTitle, location.href)
+	if (newTitle != null && newTitle.length > 0){
+		document.head.querySelector('title').text = newTitle;
+		history.replaceState(history.state, newTitle, location.href);
+	}
+	else {
+		console.warn('History not modified: New title must be a string with length >= 1.');
+	}
 })(prompt('Enter the new title for the current page:'));
